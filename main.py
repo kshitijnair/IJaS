1deb16cfd20bd05f84f766b03382c5e07ddd32cc
Kshitij Nair <kshitijnairr@gmail.com>
2020-12-14 13:35:37 +0530

    added icons

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("git uploaded! yAaay!!")
+from cv2 import cv2
+import numpy as np
+import time
+import keyboard
+
+# loading from previous setup
+prev_values = True
+# if prev_values:
+    # penval = np.load('penval.py')
+
+# video_stream = cv2.VideoCapture(0)
+
+pen_img = cv2.resize(cv2.imread('icons/pen.jpg', 1), (50,50))
+eraser_img = cv2.resize(cv2.imread('icons/eraser.jpg', 1), (50,50))
+nopen_img = cv2.resize(cv2.imread('icons/nopen.jpg', 1), (50,50))
+
+# test for icons
+# cv2.imshow("penimg", eraser_img)
+# cv2.waitKey(0)
