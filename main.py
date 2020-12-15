ec531a3e3e34ed3a31123672149eaa7ba9a7067d
Kshitij Nair <kshitijnairr@gmail.com>
2020-12-15 13:44:34 +0530

    update

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ pen_img = cv2.resize(cv2.imread('icons/pen.jpg', 1), (50,50))
 eraser_img = cv2.resize(cv2.imread('icons/eraser.jpg', 1), (50,50))
 nopen_img = cv2.resize(cv2.imread('icons/nopen.jpg', 1), (50,50))
 
+#####################
 # test for icons
 # cv2.imshow("penimg", eraser_img)
 # cv2.waitKey(0)
